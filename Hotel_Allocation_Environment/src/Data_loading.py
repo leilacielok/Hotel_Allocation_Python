3c3b2675c17e4079596a1abba0220d19b204a3ea
Leila Cielok <leila.cielok@studenti.unimi.it>
2024-11-11 16:54:16 +0100

    Modified the 'guest' column in preferences to be able to merge it with the guests dataframe

--- Hotel_Allocation_Environment/src/Data_loading.py
+++ Hotel_Allocation_Environment/src/Data_loading.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 def load_data():
-    hotels_df = pd.read_excel("C:/Users/Leila/Downloads/hotels.xlsx")
-    guests_df = pd.read_excel("C:/Users/Leila/Downloads/guests.xlsx")
-    preferences_df = pd.read_excel("C:/Users/Leila/Downloads/preferences.xlsx")
+    hotels_df = pd.read_excel("C:/Users/Leila/Downloads/hotels.xlsx", index_col=0)
+    guests_df = pd.read_excel("C:/Users/Leila/Downloads/guests.xlsx", index_col=0)
+    preferences_df = pd.read_excel("C:/Users/Leila/Downloads/preferences.xlsx", index_col=0)
+    preferences_df['guest'] = preferences_df['guest'].str.extract(r'(\d+)', expand=False).astype(int)
+
     return hotels_df, guests_df, preferences_df
 
-print(load_data())
+hotels_df, guests_df, preferences_df = load_data()
