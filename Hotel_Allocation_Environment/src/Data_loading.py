89b99611aa28f31250365aeb86fef44256ff1d86
Leila Cielok <leila.cielok@studenti.unimi.it>
2024-11-11 16:36:20 +0100

    Adding 'df' to clarify

--- Hotel_Allocation_Environment/src/Data_loading.py
+++ Hotel_Allocation_Environment/src/Data_loading.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def load_data():
-    hotels = pd.read_excel("C:/Users/Leila/Downloads/hotels.xlsx")
-    guests = pd.read_excel("C:/Users/Leila/Downloads/guests.xlsx")
-    preferences = pd.read_excel("C:/Users/Leila/Downloads/preferences.xlsx")
-    return hotels, guests, preferences
+    hotels_df = pd.read_excel("C:/Users/Leila/Downloads/hotels.xlsx")
+    guests_df = pd.read_excel("C:/Users/Leila/Downloads/guests.xlsx")
+    preferences_df = pd.read_excel("C:/Users/Leila/Downloads/preferences.xlsx")
+    return hotels_df, guests_df, preferences_df
 
 print(load_data())
